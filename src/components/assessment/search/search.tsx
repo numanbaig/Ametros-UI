1f002ca24515d0d3b06aa206aab103eecd4cad4c
Shavaiz Ali <numansafi97@gmail.com>
2025-01-23 10:53:11 +0500

    responsiveness

--- src/components/assessment/search/search.tsx
+++ src/components/assessment/search/search.tsx
@@ -6,7 +6,7 @@ const Search = () => {
   return (
     <>
       <Input
-        className="w-full md:w-[401px] sm:flex hidden border border-[#D8DAD9] pl-4 py-2 rounded-[8px] shadow-none placeholder:text-light-200 outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0  text-backgroundGrayDark font-[family-name:var(--font-poppins-semibold)] text-[16px] font-normal text-customGray-400 leading-5"
+        className="w-full lg:w-[401px] sm:flex hidden border border-[#D8DAD9] pl-4 py-2 rounded-[8px] shadow-none placeholder:text-light-200 outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0  text-backgroundGrayDark font-[family-name:var(--font-poppins-semibold)] text-[16px] font-normal text-customGray-400 leading-5"
         type="text"
         name="search"
         placeholder="Search"
