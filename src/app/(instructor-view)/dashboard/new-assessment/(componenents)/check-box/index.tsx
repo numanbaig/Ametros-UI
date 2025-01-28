f0627118538fba2c2108696307a05780328438aa
Shavaiz Ali <numansafi97@gmail.com>
2025-01-28 09:29:00 +0500

    responsiveness

--- src/app/(instructor-view)/dashboard/new-assessment/(componenents)/check-box/index.tsx
+++ src/app/(instructor-view)/dashboard/new-assessment/(componenents)/check-box/index.tsx
@@ -2,17 +2,21 @@
 
 import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
+import Link from "next/link";
 const DashboardCheckbox = () => {
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id="terms" />
-      <Label
+      <Checkbox id="terms" className="size-6 rounded-[4px p-1] text-white" />
+      <label
         htmlFor="terms"
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        className="text-[16px] text-customGray-600 font-medium leading-6"
       >
-        Accept terms and conditions
-      </Label>
+        I accept the
+        <Link href={"/"} className="text-primary-600 underline">
+          {" "}
+          Terms and Conditions
+        </Link>
+      </label>
     </div>
   );
 };
